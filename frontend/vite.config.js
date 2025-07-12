5bffcd54d63a7ca0165347fd67417f455366dadb
Yalexashishvivekchauhan <alexashishvivekchauhan@gmail.com>
2025-07-12 11:34:38 +0530

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/alexashishvivekchauhan/', // ✅ THIS MATCHES YOUR REPO NAME EXACTLY
+  base: '/alexashishvivekchauhan/',
 })
